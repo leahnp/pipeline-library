9e4024ec9d590197db115b7c38884f2025e3ca71
Marat G <maratoid@gmail.com>
2018-03-08 17:10:13 -0800

    start building out the structure, try out docker-in-docker

---
+++ src/io/cnct/pipeline/chartRepoBuilder.groovy
@@ -0,0 +1,42 @@
+// /src/io/cnct/pipeline/chartRepoBuilder.groovy
+#!/usr/bin/groovy
+package io.cnct.pipeline;
+
+chartRepoBuilder(pipelineDefinition) {
+  // Create a globally accessible variable that makes
+  // the YAML pipeline definition available to all scripts
+  pd = pipelineDefinition
+}
+
+def executePipeline() {
+  node {
+    properties(
+      [ disableConcurrentBuilds() ]
+    )
+
+    podTemplate(label: "chart-builder", containers: [
+      containerTemplate(
+        name: 'dind',
+        image: 'docker:stable-dind',
+        privileged: true,
+        alwaysPullImage: true
+      ),
+      containerTemplate(
+        name: 'docker', 
+        image: 'docker', 
+        command: 'cat', 
+        ttyEnabled: true,
+        envVars: [envVar(key: 'DOCKER_HOST', value: 'localhost:2375')]
+      )
+    ],
+    volumes: [
+      emptyDirVolume(mountPath: '/var/lib/docker', memory: false),
+    ]) {
+      stage('Build') {
+        sh "docker run busybox sh -c 'echo hi'"
+      }
+    }
+  }
+}
+
+return this
