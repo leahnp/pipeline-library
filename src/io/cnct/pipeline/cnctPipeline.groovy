1b6b9f533527060472682c6ee3a14b327a1ba1ee
Marat G <maratoid@gmail.com>
2018-03-12 15:15:29 -0700

    debug

--- src/io/cnct/pipeline/cnctPipeline.groovy
+++ src/io/cnct/pipeline/cnctPipeline.groovy
@@ -25,9 +25,12 @@ def execute() {
         
         try {
           pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yaml').text)
+          echo pipelineDefinition
         } catch(FileNotFoundException e) {
           error "${pwd()}/pipeline.yaml not found!"
         }
+
+        echo pipelineDefinition
 
         switch(pipelineDefinition.pipelineType) {
           case 'chart':
