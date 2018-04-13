3dd1d488456cc0effbf8dd3518de4a5fc6a97f52
Marat G <maratoid@gmail.com>
2018-04-12 20:26:56 -0700

    attempt to make ispathchange more reliable

--- vars/isPathChange.groovy
+++ vars/isPathChange.groovy
@@ -1,18 +1,15 @@
 #!/usr/bin/groovy
-def call(String path) {  
+def call(String path, String changeId = "") { 
+  def scriptVal = "git diff --name-only HEAD HEAD^"
+  if (changeId != "") {
+    scriptVal = """
+git fetch origin +refs/pull/${changeId}/merge
+git --no-pager diff --name-only FETCH_HEAD \$(git merge-base FETCH_HEAD master) | grep '\${path}'"""
+  }
+
   def pathChanged = sh(
     returnStatus: true,
-    script: """
-#!/usr/bin/env bash
-set -eo pipefail
-merge_commit=\"\$(git rev-parse --short HEAD)\"
-child_commit_range=\"\$(git show \"\${merge_commit}\" | grep 'Merge:' | cut -c8- | sed 's/ /../g' || true)\"
-if [ \"\${child_commit_range}\" == \"\" ]; then
-  child_commit_range=\"\${merge_commit}\"
-fi
-echo \"\${child_commit_range}\"
-git diff-tree --no-commit-id --name-only -r \"\${child_commit_range}\" | grep '${path}'
-exit \$?"""
+    script: scriptVal
   )
 
   return pathChanged
