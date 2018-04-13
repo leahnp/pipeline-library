082df911a0e4ce8f60a324805d367b35cd0def3b
Marat G <maratoid@gmail.com>
2018-04-12 20:43:00 -0700

    attempt to make ispathchange more reliable

--- vars/isPathChange.groovy
+++ vars/isPathChange.groovy
@@ -3,8 +3,11 @@ def call(String path, String changeId = "") {
   def scriptVal = "git diff --name-only HEAD HEAD^"
   if (changeId != "") {
     scriptVal = """
+#!/usr/bin/env bash
+set -eo pipefail
 git fetch origin +refs/pull/${changeId}/merge
-git --no-pager diff --name-only FETCH_HEAD \$(git merge-base FETCH_HEAD master) | grep '\${path}'"""
+git --no-pager diff --name-only FETCH_HEAD \$(git merge-base FETCH_HEAD origin/master) | grep '\${path}'
+exit \$?"""
   }
 
   def pathChanged = sh(
