8754d04c83841ae3112f1bc4071b25ef20ece8d4
Marat G <maratoid@gmail.com>
2018-04-12 20:57:32 -0700

    attempt to make ispathchange more reliable

--- vars/isPathChange.groovy
+++ vars/isPathChange.groovy
@@ -6,7 +6,7 @@ def call(String path, String changeId = "") {
 #!/usr/bin/env bash
 set -eo pipefail
 git fetch origin +refs/pull/${changeId}/merge
-git --no-pager diff --name-only FETCH_HEAD \$(git merge-base FETCH_HEAD origin/master) | grep '\${path}'
+git --no-pager diff --name-only FETCH_HEAD \$(git merge-base FETCH_HEAD origin/master) | grep '${path}'
 exit \$?"""
   }
 
