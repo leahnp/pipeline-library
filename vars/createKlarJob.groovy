ffa60d0631dd0c38ad2fc1198dc9a3b0ddc9f4cc
Leah Petersen <leahnpetersen@gmail.com>
2018-07-10 15:29:37 -0400

    idk

--- vars/createKlarJob.groovy
+++ vars/createKlarJob.groovy
@@ -24,7 +24,7 @@ def call(imageUrl) {
               [
                 "name": "klar",
                 "image": "leahnp/klar-scratch",
-                "args": imageUrl,
+                "args": imageUrl[0],
                 "env": [
                   [
                     "name": "CLAIR_ADDR",
