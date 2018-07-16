eb8ab4123a71b89fa48bfa267eea10dd420e4ef6
Marat G <maratoid@gmail.com>
2018-07-16 13:03:40 -0700

    add top level location override for docker files

--- vars/dockerfileLocation.groovy
+++ vars/dockerfileLocation.groovy
@@ -1,14 +1,7 @@
-import com.cloudbees.groovy.cps.NonCPS
-
-def call(Map defaults, String locationOverride, String packageName) {
-  return getDockerfileLocation(defaults, locationOverride, packageName)
-}
-
-@NonCPS
-def getDockerfileLocation(defaults, locationOverride, packageName) {
+def call(defaults, locationOverride, packageName) {
   if (locationOverride?.trim()) {
     if (fileExists("${locationOverride}/${packageName}/Dockerfile")) {
-      return "${locationOverride}/${packageName}/Dockerfile""
+      return "${locationOverride}/${packageName}/Dockerfile"
     } else {
       error "Could not find ${locationOverride}/${packageName}/Dockerfile"
     }
