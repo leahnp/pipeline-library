ce84a9a7a261463a55b50391553fee740b7e58bb
Marat G <maratoid@gmail.com>
2018-07-16 11:53:31 -0700

    add top level location override for docker files

--- vars/dockerfileLocation.groovy
+++ vars/dockerfileLocation.groovy
@@ -1,9 +1,17 @@
-def call(defaults, packageName) {
+def call(defaults, container) {
+  if (container.locationOverride) {
+    if (fileExists("${container.locationOverride}/${container.context}/Dockerfile")) {
+      return "${container.locationOverride}/${container.context}/Dockerfile""
+    } else {
+      error "Could not find ${container.locationOverride}/${container.context}/Dockerfile"
+    }
+  }
+
   for (loc in defaults.packages ) {
-    if (fileExists("${loc}/${packageName}/Dockerfile")) {
-      return "${loc}/${packageName}/Dockerfile"
+    if (fileExists("${loc}/${container.context}/Dockerfile")) {
+      return "${loc}/${container.context}/Dockerfile"
     }
   }   
 
-  error "Could not find ${packageName}/Dockerfile in any of the ${defaults.packages.toString()} locations"
+  error "Could not find ${container.context}/Dockerfile in any of the ${defaults.packages.toString()} locations"
 }
