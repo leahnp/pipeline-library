b4fc024b145a116f5f52d3b0a1c9295facfd1855
Leah Petersen <leahnpetersen@gmail.com>
2018-07-18 11:28:45 -0400

    template for klar job

---
+++ vars/createKlarJob.groovy
@@ -0,0 +1,43 @@
+def call(imageUrl, maxCve, maxLevel) {
+  echo("Creating job template to scan ${imageUrl} for vulnerabilities")
+  def klarJobYaml = [
+      "apiVersion": "batch/v1",
+      "kind": "Job",
+      "metadata": [
+        "name": "klar"
+      ],
+      "spec": [
+        "template": [
+          "spec": [
+            "restartPolicy": "Never",
+            "containers": [
+              [
+                "name": "klar",
+                "image": "quay.io/samsung_cnct/klar:0.0.2-a0ca0f21c6d44e437c76355913e2937e8d242030",
+                "args": [
+                  imageUrl
+                ],
+                "env": [
+                  [
+                    "name": "CLAIR_ADDR",
+                    "value": "clairsvc:6060"
+                  ],
+                  [
+                    "name": "CLAIR_OUTPUT",
+                    "value": maxLevel
+                  ],
+                  [
+                    "name": "CLAIR_THRESHOLD",
+                    "value": maxCve
+                  ]
+                ]
+              ]
+            ],
+          ]
+        ],
+        "backoffLimit": 0
+      ]
+    ]
+
+  return klarJobYaml
+}
