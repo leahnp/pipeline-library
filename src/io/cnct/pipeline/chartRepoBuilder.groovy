5bb1b022b349aa65e562bb14122c8a0059e05f64
Leah Petersen <leahnpetersen@gmail.com>
2018-07-20 15:25:08 -0400

    testing

--- src/io/cnct/pipeline/chartRepoBuilder.groovy
+++ src/io/cnct/pipeline/chartRepoBuilder.groovy
@@ -470,8 +470,8 @@ def buildsTestHandler(scmVars) {
 
       for (container in pipeline.builds) {
         
-        def jobName = "klar-${kubeName(env.JOB_NAME)-(container.image)}"
-        echo("jobName")
+        def jobName = "klar-${kubeName(container.image)-(env.JOB_NAME)}"
+        echo("dogz")
         echo(jobName)
         
 
